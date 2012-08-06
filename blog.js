fea61fc5045e29611e858bd7191b6116f7684969
Adam Krone <krone.adam@gmail.com>
2012-08-06 15:41:57 -0500

    Added/refined basic post functionality

--- blog.js
+++ blog.js
@@ -19,31 +19,51 @@ if (Meteor.is_client) {
             date = month + "-" + day + "-" + year,
             content = $('#editor .content').val();
         Posts.insert({title: title, date: date, content: content});
-        $("#editor").slideUp('slow');
-        $("#add_post").show();
+        $('#editor').slideUp('slow');
+        $('#add_post').show();
       },
       'click #add_post': function () {
-        $("#editor").slideDown('slow');
-        $("#add_post").hide();
+        $('#editor').slideDown('slow');
+        $('#add_post').hide();
+      },
+      'click #cancel_post': function () {
+        $("#editor").slideUp('slow');
+        $('#add_post').show();
       }
     };
 
     //Posts
 
     Template.posts.posts = function () {
-      return Posts.find();
+      return Posts.find({}, {sort: {_id: 1}});
     };
 
     Template.posts.events = {
-      'click .update_post': function () {
+      'click .update_post': function (event) {
         var target = event.currentTarget,
             id = $(target).parent().attr('id'),
             title = $(target).siblings('.title').text(),
             date = $(target).siblings('h2').find('.date').text(),
             content = $(target).siblings('.content').text();
         Posts.update({_id: id}, {title: title, date: date, content: content});
+      },
+      'click .delete_post': function (event) {
+        var target = event.currentTarget,
+            id = $(target).parent().attr('id'),
+            title = $(target).siblings('.title').text(),
+            confirm = window.confirm("Delete post: " + title + "?");
+        if (confirm) {
+            Posts.remove(id);
+        }
       }
     };
+
+    //Sidebar
+
+    Template.sidebar.posts = function () {
+      var posts = Posts.find({}, {sort: {_id: 1}}).fetch();
+      return posts.slice(0, 5);
+    }
 
     //Footer
 }
