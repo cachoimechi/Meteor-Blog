c57f1be7f7f1fcf428930a4e152afb38efe65e46
Adam Krone <krone.adam@gmail.com>
2012-08-07 15:39:44 -0500

    Adds basic page functionality, including /admin

--- blog.js
+++ blog.js
@@ -22,23 +22,36 @@ if (Meteor.is_client) {
         $('#editor').slideUp('slow');
         $('#add_post').show();
       },
-      'click #add_post': function () {
+      'click #add_post': function (event) {
+        event.preventDefault();
         $('#editor').slideDown('slow');
         $('#add_post').hide();
       },
-      'click #cancel_post': function () {
+      'click #cancel_post': function (event) {
+        event.preventDefault();
         $("#editor").slideUp('slow');
         $('#add_post').show();
       }
     };
 
     //Posts
+    
+    Template.posts.one_post = function () {
+      return !Session.equals('post_id', undefined);
+    }
+
+    Template.posts.is_admin = function () {
+      return Session.equals('is_admin', true);
+    }
 
     Template.posts.posts = function () {
       return Posts.find({}, {sort: {_id: 1}});
     };
 
     Template.posts.events = {
+      /*'mousedown .post': function (evt) {
+        Router.setPost(this._id);
+      },*/
       'click .update_post': function (event) {
         var target = event.currentTarget,
             id = $(target).parent().attr('id'),
@@ -58,6 +71,30 @@ if (Meteor.is_client) {
       }
     };
 
+    Template.single_post.is_admin = function () {
+      return Session.equals('is_admin', true);
+    }
+
+    Template.single_post.id = function () {
+      var post = Posts.findOne(Session.get('post_id'));
+      return post && post._id;
+    }
+
+    Template.single_post.title = function () {
+      var post = Posts.findOne(Session.get('post_id'));
+      return post && post.title;
+    };
+
+    Template.single_post.date = function () {
+      var post = Posts.findOne(Session.get('post_id'));
+      return post && post.date;
+    };
+
+    Template.single_post.content = function () {
+      var post = Posts.findOne(Session.get('post_id'));
+      return post && post.content;
+    };
+
     //Sidebar
 
     Template.sidebar.posts = function () {
@@ -66,10 +103,43 @@ if (Meteor.is_client) {
     }
 
     //Footer
+
+    /**
+     * Routers
+     */
+
+    var BlogRouter = Backbone.Router.extend({
+      routes: {
+        "admin": "admin",
+        "admin/:post_id": "admin_post",
+        ":post_id": "main"
+      },
+      admin: function () {
+        Session.set('is_admin', true);
+      },
+      admin_post: function (post_id) {
+        Session.set('post_id', post_id);
+        Session.set('is_admin', true);
+      },
+      main: function (post_id) {
+        Session.set('post_id', post_id);
+        Session.set('is_admin', false);
+      }
+    });
+
+    Router = new BlogRouter;
+
+    Meteor.startup(function () {
+
+      Backbone.history.start({pushState: true});
+    });
+
 }
 
+/**
+ * Server
+ */
+
 if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
 }
